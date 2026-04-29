a22abc27959ca0e47377377bbe3c154a48adbaa7
Joseph Wang <ivan_whisper@outlook.com>
2026-04-29 16:49:20 +0800

    add

--- go.mod
+++ go.mod
@@ -1,20 +1,25 @@
 module github.com/illidaris/diplomatist
 
-go 1.18
+go 1.24.0
+
+toolchain go1.24.8
 
 require (
 	github.com/google/go-querystring v1.1.0
 	github.com/illidaris/aphrodite v0.4.7
+	github.com/illidaris/core v1.0.0
 	github.com/illidaris/rest v1.4.5
+	github.com/larksuite/oapi-sdk-go/v3 v3.5.3
+	github.com/oklog/ulid/v2 v2.1.1
 	github.com/smartystreets/goconvey v1.8.1
+	github.com/spf13/cast v1.5.0
+	golang.org/x/oauth2 v0.35.0
 )
 
 require (
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
-	github.com/illidaris/core v1.0.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/smarty/assertions v1.15.0 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 )
--- usdk.dailygn.com/dto/test_group_data.go
+++ usdk.dailygn.com/dto/test_group_data.go
@@ -8,10 +8,10 @@ import (
 
 type TestGroupDataListRequest struct {
 	BaseDateRequest
-	UserType   int64  `json:"user_type"`   // 0:官包对应的game_user_id 1:抖音流水分账包对应的sdk_open_id
-	GroupId    string `json:"group_id"`    // 需要查询实验信息的日期，该时间为需要统计汇总付费的日期，刷新时间一般为T+1，日期格式：20240730
-	PageSize   int64  `json:"page_size"`   // 分页大小，最大值为10000，如果失败请求过多建议调小
-	PageOffset int64  `json:"page_offset"` // 分页下标，起始值为0
+	UserType   int64  `json:"user_type" form:"user_type" url:"user_type"`       // 0:官包对应的game_user_id 1:抖音流水分账包对应的sdk_open_id
+	GroupId    string `json:"group_id" form:"group_id" url:"group_id"`          // 需要查询实验信息的日期，该时间为需要统计汇总付费的日期，刷新时间一般为T+1，日期格式：20240730
+	PageSize   int64  `json:"page_size" form:"page_size" url:"page_size"`       // 分页大小，最大值为10000，如果失败请求过多建议调小
+	PageOffset int64  `json:"page_offset" form:"page_offset" url:"page_offset"` // 分页下标，起始值为0
 }
 
 func (r TestGroupDataListRequest) GetUrlQuery() url.Values {
